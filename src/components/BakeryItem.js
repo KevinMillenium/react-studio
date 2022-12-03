dd9cd6ab12b1064544f6c19397c4b8ae37e1f256
KevinWXYStudios <85246158+KevinWXYStudios@users.noreply.github.com>
2022-12-03 17:12:15 -0500

    yes

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,12 @@
 // TODO: create a component that displays a single bakery item
+export default function BakeryItem(props) {
+	return (
+      <div className="bakery-item">
+        <img className="image" src={props.item.image}></img>
+        <h3>{props.item.name}</h3>
+        <p>{props.item.description}</p>
+        <p>{props.item.price}</p>
+        <img>{props.item.image}</img>
+      </div>
+    );
+}
